376d1419b6e234556a655ac9782ee2d865e778b7
Jan Kowalski <jan@kowalski.pl>
2025-11-27 12:38:08 +0100

    com

--- api/Models/TodoItem.cs
+++ api/Models/TodoItem.cs
@@ -6,6 +6,6 @@ public class TodoItem
     public string Title {get;set;} = default!;
     public bool IsDone {get;set;}
     public DateTime CreatedAt {get;set;} = DateTime.UtcNow;
-
+    public DateTime ? Deadline {get;set;}
     public int Priority {get; set;} = 1;
 }
